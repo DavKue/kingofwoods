89f49baf4eddbb6ee1e701f2a0bc6937f0ad6c93
DavKue <david@schusterfilm.de>
2025-03-05 00:31:31 +0100

    Spritesheet and Materials

---
+++ material.inc.php
@@ -0,0 +1,57 @@
+$this->cards = [
+ 'C01' => [
+   'name' => clienttranslate("Assassin"),
+   'influence' => 0,
+  ],
+
+  'C02' => [
+   'name' => clienttranslate("Trader"),
+   'influence' => 1,
+  ],
+
+  'C03' => [
+   'name' => clienttranslate("Guard"),
+   'influence' => 2,
+  ],
+
+  'C04' => [
+   'name' => clienttranslate("Squire"),
+   'influence' => 2,
+  ],
+
+  'C05' => [
+   'name' => clienttranslate("Scholar"),
+   'influence' => 3,
+  ],
+
+  'C06' => [
+   'name' => clienttranslate("Priest"),
+   'influence' => 3,
+  ],
+
+  'C07' => [
+   'name' => clienttranslate("Jester"),
+   'influence' => 4,
+  ],
+
+  'C08' => [
+   'name' => clienttranslate("Treasurer"),
+   'influence' => 4,
+  ],
+
+  'C09' => [
+   'name' => clienttranslate("Knight"),
+   'influence' => 5,
+  ],
+
+  'C10' => [
+   'name' => clienttranslate("General"),
+   'influence' => 6,
+  ],
+
+  'C11' => [
+   'name' => clienttranslate("Princess"),
+   'influence' => 7,
+  ],
+
+];
