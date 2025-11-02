e4c12aa135e7153e3a2a4978d042eee549616d32
DavKue <david@schusterfilm.de>
2025-11-02 21:28:33 +0100

    Tech Changes due to log

--- modules/php/Game.php
+++ modules/php/Game.php
@@ -1976,16 +1976,6 @@ class Game extends \Table
     }
 
     /**
-     * Returns the game name.
-     *
-     * IMPORTANT: Please do not modify.
-     */
-    protected function getGameName()
-    {
-        return "thekingofthewoods";
-    }
-
-    /**
      * This method is called only once, when a new game is launched. In this method, you must setup the game
      *  according to the game rules, so that the game is ready to be played.
      */
